db3d7b63304556494ad6544ef4c1fc5b4ef488f5
Greg Schuman <greg.c.schuman@gmail.com>
2026-06-13 14:50:15 -0700

    In the types/Scenarios.ts component, I added what composes of a social scenario. Things like expected outcomes, social norms, anticipated chhallenges, etc.

---
+++ src/types/Scenario.ts
@@ -0,0 +1,13 @@
+export type Scenario = {
+  id: string
+  title: string
+  category: string
+  description: string
+  expectedOutcomes: string[]
+  conversationStarters: string[]
+  socialNorms: string[]
+  anticipatedChallenges: string[]
+  misunderstandings: string[]
+  exitStrategies: string[]
+  resources: string[]
+}
