2a7cd7d1d873f25ac7c5be88795d22b9695e82d1
Greg Schuman <greg.c.schuman@gmail.com>
2026-06-13 15:54:29 -0700

    Added ScenarioList components.

---
+++ src/components/ScenarioList.tsx
@@ -0,0 +1,21 @@
+// A list of scenarios with their titles and descriptions
+
+import type { Scenario } from "../types/Scenario"
+import { ScenarioCard } from "./ScenarioCard"
+
+interface ScenarioListProps {
+  scenarios: Scenario[]
+}
+
+export const ScenarioList: React.FC<ScenarioListProps> = ({
+  scenarios,
+}) => {
+  return (
+    <div className="space-y-4">
+      {scenarios.map((scenario) => (
+        <ScenarioCard key={scenario.id} 
+        scenario={scenario} />
+      ))}
+    </div>
+  )
+}
